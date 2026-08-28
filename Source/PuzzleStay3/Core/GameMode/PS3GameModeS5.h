c69892aa67311f4de01b72acc4598eef18fe6544
mnns0805 <mnns0805@gmail.com>
2026-08-28 17:44:26 +0900

    feat: GameMode 클래스 추가

---
+++ Source/PuzzleStay3/Core/GameMode/PS3GameModeS5.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PS3GamemodeBase.h"
+#include "PS3GameModeS5.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLESTAY3_API APS3GameModeS5 : public APS3GamemodeBase
+{
+	GENERATED_BODY()
+};
