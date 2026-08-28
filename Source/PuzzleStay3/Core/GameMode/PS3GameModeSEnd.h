210f8225253a3da7a22c85c100648d153e99a8d8
mnns0805 <mnns0805@gmail.com>
2026-08-28 17:46:14 +0900

    feat: GameModeEnd 클래스 추가

---
+++ Source/PuzzleStay3/Core/GameMode/PS3GameModeSEnd.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PS3GamemodeBase.h"
+#include "PS3GameModeSEnd.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLESTAY3_API APS3GameModeSEnd : public APS3GamemodeBase
+{
+	GENERATED_BODY()
+};
