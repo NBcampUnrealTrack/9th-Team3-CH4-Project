ca24a5d3574830abfc74e807ab643ce1b0904b63
mnns0805 <mnns0805@gmail.com>
2026-08-28 19:16:04 +0900

    feat: PS3GameStateBase 클래스 추가

---
+++ Source/PuzzleStay3/Core/GameState/PS3GameStateBase.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "PS3GameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLESTAY3_API APS3GameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+};
