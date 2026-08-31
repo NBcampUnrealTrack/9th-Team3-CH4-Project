8512f07aaa884c7607ca9a0f62712c7e5b3ff954
Noong <mw.noong@gmail.com>
2026-08-31 14:27:45 +0900

    Feat(object): BlockingVolume 비활성화 로직 추가
    GameMode 이벤트와 연동해 BlockingVolume 충돌을 비활성화하는 로직 구현 추가.

---
+++ Source/PuzzleStay3/Object/PS3BlockingVolume.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/BlockingVolume.h"
+#include "PS3BlockingVolume.generated.h"
+
+UCLASS()
+class PUZZLESTAY3_API APS3BlockingVolume : public ABlockingVolume
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	UFUNCTION()
+	void BlockingVolumeDisabled();
+};
