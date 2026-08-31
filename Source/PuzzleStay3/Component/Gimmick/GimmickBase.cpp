e41abcc4b7c80185026feacd4d4ea9809f8d1b18
limichcutie <rudghks238@gmail.com>
2026-08-31 11:41:06 +0900

    GimmickBase초기세팅

---
+++ Source/PuzzleStay3/Component/Gimmick/GimmickBase.cpp
@@ -0,0 +1,54 @@
+#include "GimmickBase.h"
+
+#include "Components/SceneComponent.h"
+#include "Net/UnrealNetwork.h"
+
+AGimmickBase::AGimmickBase()
+{
+ 	bReplicates = true;
+	SetReplicateMovement(false);
+	
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+}
+
+void AGimmickBase::BeginPlay()
+{
+	Super::BeginPlay();	
+}
+
+bool AGimmickBase::bCanInteract(const APawn* InteractingPawn) const
+{
+	return bIsActive && !bIsCompleted && IsValid(InteractingPawn);
+}
+
+void AGimmickBase::HandleInteraction(APawn* InteractingPawn)
+{
+	if (!HasAuthority() || !bCanInteract(InteractingPawn))
+	{
+		return;
+	}
+
+	BP_OnInteraction(InteractingPawn);
+}
+
+void AGimmickBase::OnRep_IsActive()
+{
+	BP_OnActiveStateChanged(bIsActive);
+}
+
+void AGimmickBase::OnRep_IsCompleted()
+{
+	BP_OnCompleted();
+}
+
+void AGimmickBase::GetLifetimeReplicatedProps(
+	TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AGimmickBase, bIsActive);
+	DOREPLIFETIME(AGimmickBase, bIsCompleted);
+}
+
