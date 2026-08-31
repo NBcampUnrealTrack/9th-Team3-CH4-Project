f77f437f1791bad030426e3dc5a97669b0cd06dd
mnns0805 <mnns0805@gmail.com>
2026-08-31 18:21:59 +0900

    feat: 스테이지5 게임모드 작업 중

--- Source/PuzzleStay3/Core/GameMode/PS3GameModeS5.cpp
+++ Source/PuzzleStay3/Core/GameMode/PS3GameModeS5.cpp
@@ -3,12 +3,25 @@
 
 #include "PS3GameModeS5.h"
 
+#include "GameFramework/GameSession.h"
+#include "GameFramework/PlayerState.h"
+#include "PuzzleStay3/Core/GameInstance/PS3GameInstance.h"
+#include "PuzzleStay3/Player/Controller/PS3ChoiceController.h"
+
 void APS3GameModeS5::BeginPlay()
 {
 	Super::BeginPlay();
 	
 	GetWorld()->GetTimerManager().SetTimer(GameLimitTimeHandle, this, &ThisClass::OnReduceGameTime, 1.f, true);
 }
+
+void APS3GameModeS5::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+	
+	SetPlayerRole(NewPlayer);
+}
+
 
 
 void APS3GameModeS5::OnReduceGameTime()
@@ -25,3 +38,35 @@ void APS3GameModeS5::OnReduceGameTime()
 	}
 }
 
+
+void APS3GameModeS5::SetPlayerRole(const APlayerController* CurrentPlayer)
+{
+	if (IsValid(CurrentPlayer) == false) return;
+	if (IsValid(CurrentPlayer->PlayerState) == false) return;
+	
+	const FUniqueNetIdRepl& PlayerID = CurrentPlayer->PlayerState->GetUniqueId();
+	if (PlayerID.IsValid() == false) return;
+	
+	auto* PS3GameInstance = Cast<UPS3GameInstance>(GetGameInstance());
+	if (IsValid(PS3GameInstance) == false) return; 
+	
+	EPS3PlayerRoleType CurrentPlayerRoleType = PS3GameInstance->GetControllerEType(PlayerID);
+	
+	if (CurrentPlayerRoleType == EPS3PlayerRoleType::PlayerRole_ThirdPerson)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Green,
+		FString::Printf(TEXT("3인칭조작 컨트롤러 할당 해야함")));
+	}
+	
+	if (CurrentPlayerRoleType == EPS3PlayerRoleType::PlayerRole_Screen)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Blue,
+			FString::Printf(TEXT("스크린조작 컨트롤러 할당 해야함")));
+	}	
+	
+	if (CurrentPlayerRoleType == EPS3PlayerRoleType::PlayerRole_None)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red,
+	FString::Printf(TEXT("UI 컨트롤러 할당 해야함")));
+	}
+}
