cf29aa68f8745ae17a0a66d2a280a6aa8f637fe7
Noong <mw.noong@gmail.com>
2026-09-02 01:51:38 +0900

    Feat(component): BlockingVolume 비활성화 로직 개선
    Stage1/Stage3 컴포넌트의 Disable 함수에 스테이지 번호 기반 조건 추가.
    DisableBlockingVolumeComponent가 GameMode와 연동해 Player1 오버랩 시 Stage1 블록 볼륨 비활성화 처리.
    
    ⚠ 에셋과 코드 변경 분리 권장

--- Source/PuzzleStay3/Component/DisableBlockingVolumeComponent.cpp
+++ Source/PuzzleStay3/Component/DisableBlockingVolumeComponent.cpp
@@ -1,27 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "DisableBlockingVolumeComponent.h"
+
+#include "Core/GameMode/PS3GameModeBase.h"
+#include "Kismet/GameplayStatics.h"
 
 
 UDisableBlockingVolumeComponent::UDisableBlockingVolumeComponent()
 {
-	PrimaryComponentTick.bCanEverTick = true;
-}
-
-bool UDisableBlockingVolumeComponent::IsOverlapped() const
-{
-	return bIsOverlapped;
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UDisableBlockingVolumeComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	OnComponentBeginOverlap.AddDynamic(this, &UDisableBlockingVolumeComponent::OnOverlapDeathVolume);
+	OnComponentBeginOverlap.AddDynamic(this, &UDisableBlockingVolumeComponent::OnOverlapDisableBlockingVolume);
 }
 
-void UDisableBlockingVolumeComponent::OnOverlapDeathVolume(
+void UDisableBlockingVolumeComponent::OnOverlapDisableBlockingVolume(
 	UPrimitiveComponent* OverlappedComp,
 	AActor* OtherActor,
 	UPrimitiveComponent* OtherComp,
@@ -30,5 +26,17 @@ void UDisableBlockingVolumeComponent::OnOverlapDeathVolume(
 	const FHitResult& SweepResult
 	)
 {
+	if (bIsOverlapped) return;
+	
+	APawn* overlappedPawn = Cast<APawn>(OtherActor);
+	if (!IsValid(overlappedPawn) || !overlappedPawn->IsPlayerControlled()) return;
+	
 	bIsOverlapped = true;
+	
+	APS3GameModeBase* GameMode = Cast<APS3GameModeBase>(UGameplayStatics::GetGameMode(this));
+	if (!IsValid(GameMode)) return;
+	
+	//플레이어 오버랩 시 게임모드의 DisableBlockingVolume 함수를 호출
+	GameMode->DisableBlockingVolume(EPS3StageNumber::Stage1);
+	
 }
