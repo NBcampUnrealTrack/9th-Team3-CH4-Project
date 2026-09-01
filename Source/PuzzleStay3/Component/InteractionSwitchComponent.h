c6fd44c1626b5d30d7477cac0472efde27bd0e8d
wonbin5836 <wonbin5836@gmail.com>
2026-09-01 19:13:28 +0900

    Feat(Component): GameMode 등록 옵션 및 스위치 리셋 기능 추가
    - bRegisterToGameMode 변수로 GameMode 등록 여부 제어 가능
    - ResetSwitch 함수로 스위치 상태 수동 리셋 지원
    - BeginPlay/EndPlay 로직에 bRegisterToGameMode 조건 추가

--- Source/PuzzleStay3/Component/InteractionSwitchComponent.h
+++ Source/PuzzleStay3/Component/InteractionSwitchComponent.h
@@ -15,6 +15,7 @@ class PUZZLESTAY3_API UInteractionSwitchComponent : public UActorComponent
 public:
 	UInteractionSwitchComponent();
 
+	
 protected:
 	virtual void BeginPlay() override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
@@ -24,6 +25,11 @@ protected:
 	UPROPERTY(ReplicatedUsing = OnRep_IsActivated, VisibleAnywhere, BlueprintReadOnly, Category = "Gimmick")
 	bool bIsActivated = false;
 
+	// GameMode의 글로벌 퍼즐 스위치 목록에 등록할지 여부
+	// 일반 퍼즐 스위치는 true, 저울 버튼 등 독립 기믹 스위치는 false로 설정
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gimmick")
+	bool bRegisterToGameMode = true;
+	
 public:
 	// GameMode가 바인딩할 델리게이트
 	FOnSwitchActivatedChanged OnSwitchActivatedChanged;
@@ -33,11 +39,13 @@ public:
 	bool IsActivated() const { return bIsActivated; }
 
 	// PlayerCharacter F키 라인트레이스 수신 인터페이스에서 호출할 함수
-
-	// 서버 권위 진입점. RPC가 아님 — 캐릭터 쪽에서 이미 검증된 서버 RPC를 통해
-	// 넘어온 뒤에만 호출되어야 함. 반환값은 실제로 토글이 일어났는지 여부.
 	bool TryInteract(AActor* Requestor);
+	
+	void SetRegisterToGameMode(bool bRegister) { bRegisterToGameMode = bRegister; }
 
+	// 수동으로 스위치 상태를 Off(false)로 리셋하는 함수
+	void ResetSwitch();
+	
 protected:
 	UFUNCTION()
 	void OnRep_IsActivated();
