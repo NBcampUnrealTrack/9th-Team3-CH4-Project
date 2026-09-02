20315ad6a3bec33e22755ffaee929b85f936cdbc
Noong <mw.noong@gmail.com>
2026-09-02 14:44:51 +0900

    Feat(core): Stage1 GameMode 클래스 주석 추가
    B기믹 타이머 관련 구현 예정 내용 주석 추가로 가독성 및 추후 개발 계획 명시.

---
+++ Source/PuzzleStay3/Core/GameMode/PS3GameModeS1.h
@@ -0,0 +1,14 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PS3GameModeBase.h"
+#include "PS3GameModeS1.generated.h"
+
+//B기믹 타이머관련 예정
+
+UCLASS()
+class PUZZLESTAY3_API APS3GameModeS1 : public APS3GameModeBase
+{
+	GENERATED_BODY()
+};
