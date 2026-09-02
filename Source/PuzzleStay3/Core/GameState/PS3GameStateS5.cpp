f4eb2728e33a3349a075378299b689db8aa2c38c
mnns0805 <mnns0805@gmail.com>
2026-09-02 13:21:43 +0900

    fix: 게임스테이트 이름 변경 (사유: 각 스테이지별로 게임스테이트 개설 예정)

---
+++ Source/PuzzleStay3/Core/GameState/PS3GameStateS5.cpp
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PS3GameStateBase.h"
+
+#include "Core/GameMode/PS3GameModeS5.h"
+#include "Net/UnrealNetwork.h"
+
+
+void APS3GameStateBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(ThisClass, GameLimitTime);
+}
+
+
+void APS3GameStateBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	
+}
+
+
+void APS3GameStateBase::OnGameStart()
+{
+	GetWorld()->GetTimerManager().SetTimer(GameLimitTimeHandle, this, &ThisClass::OnReduceGameTime, 1.f, true);
+}
+
+void APS3GameStateBase::OnGameOver()
+{
+	//TODO 게임오버 UI로 변경
+	auto* PS3GameModeS5 = Cast<APS3GameModeS5>(GetWorld()->GetAuthGameMode());
+	if (IsValid(PS3GameModeS5) == false) return;
+	UE_LOG(LogTemp, Warning, TEXT("게임모드 Cast 완료. OnGameOver() 실행가능."));
+		
+	PS3GameModeS5->OnGameOver();
+}
+
+void APS3GameStateBase::OnReduceGameTime()
+{
+	--GameLimitTime;
+	
+	if (GameLimitTime <= 0.0f)
+	{
+		GetWorld()->GetTimerManager().ClearTimer(GameLimitTimeHandle);
+		UE_LOG(LogTemp, Warning, TEXT("제한시간 종료."));
+		
+		bIsGameOver = true;
+	}
+}
+
+
+void APS3GameStateBase::OnRep_IsGameOver()
+{
+	if (bIsGameOver == false) return;
+	
+	OnGameOver();
+	
+}
---
+++ Source/PuzzleStay3/Core/GameState/PS3GameStateS5.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "PS3GameStateBase.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLESTAY3_API APS3GameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	
+public:
+	void OnRep_IsGameOver();
+	
+	
+protected:
+	void OnGameStart();
+	void OnGameOver();
+	void OnReduceGameTime();
+	
+public:
+	UPROPERTY(Replicated, EditAnywhere, Category = "GameRule")
+	float GameLimitTime = 60.0f;
+	
+	UPROPERTY(ReplicatedUsing = OnRep_IsGameOver)
+	bool bIsGameOver = false;
+	
+	
+protected:
+	FTimerHandle GameLimitTimeHandle;
+	
+	
+
+
+};
