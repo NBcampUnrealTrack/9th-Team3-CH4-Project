dd55d67bfd08e89bd81d52abaf8257225d2e8c0a
Noong <mw.noong@gmail.com>
2026-09-02 14:44:51 +0900

    Feat(core): Stage1 GameMode 클래스 추가
    Stage1 전용 GameMode 클래스(PS3GameModeS1) 헤더 및 소스 파일 생성.

---
+++ Source/PuzzleStay3/Core/GameMode/PS3GameModeS1.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PS3GameModeBase.h"
+#include "PS3GameModeS1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLESTAY3_API APS3GameModeS1 : public APS3GameModeBase
+{
+	GENERATED_BODY()
+};
