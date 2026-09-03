7c97b5c3e3e0d902a292f243c84c6eff926478ff
Hwang_Junhyun <h010617@naver.com>
2026-09-03 09:54:54 +0900

    feat(Player): 덤벨 held, drop 덤벨을 이동하는 Carryanchor 소켓 추가

--- Source/PuzzleStay3/Player/Character/PS3PlayerCharacter.h
+++ Source/PuzzleStay3/Player/Character/PS3PlayerCharacter.h
@@ -9,6 +9,8 @@ class UDXHPTextWidgetComponent;
 class UDXStatusComponent;
 class UCameraComponent;
 class USpringArmComponent;
+class ADumbbell;
+class USceneComponent;
 
 UCLASS()
 class PUZZLESTAY3_API APS3PlayerCharacter : public ACharacter
@@ -21,6 +23,12 @@ public:
 
 	UFUNCTION(BlueprintPure, Category = "PS3|Character")
 	bool CanUseFieldControls() const;
+	
+	UFUNCTION(BlueprintPure, Category = "PS3|Character|Interaction")
+	USceneComponent* GetCarryAnchor() const
+	{
+		return CarryAnchor;
+	}
 
 	void Move(const FVector2D& InMovementVector);
 	void Look(const FVector2D& InLookVector);
@@ -37,9 +45,17 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "PS3|Character|Components")
 	TObjectPtr<UCameraComponent> Camera;
 
+	//상호작용할 라인트레이스 거리
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PS3|Character|Interaction")
 	float InteractionDistance = 300.0f;
 
+	//덤벨 held, drop을 위한 변수
+	UPROPERTY(Transient)
+	TObjectPtr<ADumbbell> HeldDumbbell;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = "PS3|Character|Interaction")
+	TObjectPtr<USceneComponent> CarryAnchor;
+	
 private:
 	UFUNCTION(Server, Reliable)
 	void Server_TryInteract();
