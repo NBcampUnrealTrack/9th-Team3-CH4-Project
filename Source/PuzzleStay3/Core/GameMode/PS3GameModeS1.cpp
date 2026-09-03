334c462e891302e6c10e8bd9f96548e5d74bfb99
Noong <mw.noong@gmail.com>
2026-09-03 14:26:19 +0900

    Feat(core): Stage1 블록볼륨 동기화 및 활성화 로직 추가
    Stage1 GameState 분리 및 BlockingVolume 상태 관리 기능 구현.
    Replication 기반 OnRep 함수와 델리게이트 추가로 상태 동기화 및 이벤트 처리 지원.

--- Source/PuzzleStay3/Core/GameMode/PS3GameModeS1.cpp
+++ Source/PuzzleStay3/Core/GameMode/PS3GameModeS1.cpp
@@ -3,4 +3,15 @@
 
 #include "PS3GameModeS1.h"
 
+#include "Core/GameState/PS3GameStateS1.h"
 
+
+void APS3GameModeS1::DisableBlockingVolume(EPS3StageNumber StageNumber)
+{
+	if (StageNumber != EPS3StageNumber::Stage1) return;
+
+	APS3GameStateS1* GS = GetGameState<APS3GameStateS1>();
+	if (!IsValid(GS)) return;
+
+	GS->SetStage1BlockingVolumeDisabled(true);
+}
---
+++ Source/PuzzleStay3/Core/GameState/PS3GameStateS1.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PS3GameStateBase.h"
+#include "PS3GameStateS1.generated.h"
+//DECLARE_MULTICAST_DELEGATE_OneParam(FOnBlockingVolumeDisabled, EPS3StageNumber);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnStage1BlockingVolumeDisabled, bool, bDisabled);
+
+UCLASS()
+class PUZZLESTAY3_API APS3GameStateS1 : public APS3GameStateBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UFUNCTION(BlueprintPure)
+	bool IsStage1BlockingVolumeDisabled() const { return bStage1BlockingVolumeDisabled; }
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly)
+	void SetStage1BlockingVolumeDisabled(bool bDisabled);
+
+	UPROPERTY(BlueprintAssignable)
+	FOnStage1BlockingVolumeDisabled OnStage1BlockingVolumeDisabled;
+
+protected:
+	UPROPERTY(ReplicatedUsing = OnRep_Stage1BlockingVolumeDisabled)
+	bool bStage1BlockingVolumeDisabled = false;
+
+	UFUNCTION()
+	void OnRep_Stage1BlockingVolumeDisabled();
+};
