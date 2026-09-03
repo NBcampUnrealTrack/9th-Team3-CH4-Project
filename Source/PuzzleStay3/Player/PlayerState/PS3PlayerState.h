2d145a11e9b46cb9a22a9bae6300bfc0ba3d44e3
Hwang_Junhyun <h010617@naver.com>
2026-09-03 16:58:14 +0900

    feat(player-state): 보이스챗 및 무전기 보유 상태 복제 추가
    - VoiceChatState 복제 변수와 변경 델리게이트 추가
    - 무전기 보유 상태와 Setter/Getter 추가
    - 서버 및 클라이언트 상태 변경 이벤트 처리

--- Source/PuzzleStay3/Player/PlayerState/PS3PlayerState.h
+++ Source/PuzzleStay3/Player/PlayerState/PS3PlayerState.h
@@ -2,12 +2,15 @@
 
 #include "CoreMinimal.h"
 #include "Data/Enum/PS3PlayerRole.h"
+#include "Data/Enum/VoiceChatState.h"
 #include "GameFramework/PlayerState.h"
 #include "PS3PlayerState.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPS3LifeCountChangedSignature, int32, NewLifeCount);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPS3DeadStateChangedSignature, bool, bNewIsDead);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPS3PlayerRoleChangedSignature, EPS3PlayerRole, NewRole);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPS3VoiceChatStateChangedSignature, EVoiceChatState, NewState);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPS3VoiceObjectHeldChangedSignature, bool, bNewIsHeld);
 
 UCLASS()
 class PUZZLESTAY3_API APS3PlayerState : public APlayerState
@@ -27,7 +30,20 @@ public:
 
 	UFUNCTION(BlueprintPure, Category = "PS3|Player State")
 	EPS3PlayerRole GetPlayerRole() const { return PlayerRole; }
+	
+	UFUNCTION(BlueprintPure, Category = "PS3|Player State|Voice")
+	EVoiceChatState GetVoiceChatState() const{ return VoiceChatState;}
 
+	UFUNCTION(BlueprintPure, Category = "PS3|Player State|Voice")
+	bool IsVoiceObjectHeld() const{ return bIsVoiceObjectHeld;}
+	
+	UFUNCTION( BlueprintCallable,BlueprintAuthorityOnly,Category = "PS3|Player State|Voice")
+	void SetVoiceChatState(EVoiceChatState NewState);
+
+	UFUNCTION(BlueprintCallable,BlueprintAuthorityOnly,Category = "PS3|Player State|Voice")
+	void SetVoiceObjectHeld(bool bNewIsHeld);
+	
+	
 	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "PS3|Player State")
 	void SetCurrentLifeCount(int32 NewLifeCount);
 
@@ -45,6 +61,12 @@ public:
 
 	UPROPERTY(BlueprintAssignable, Category = "PS3|Player State|Events")
 	FPS3PlayerRoleChangedSignature OnPlayerRoleChanged;
+	
+	UPROPERTY(BlueprintAssignable, Category = "PS3|Player State|Voice")
+	FPS3VoiceChatStateChangedSignature OnVoiceChatStateChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "PS3|Player State|Voice")
+	FPS3VoiceObjectHeldChangedSignature OnVoiceObjectHeldChanged;
 
 protected:
 	UFUNCTION()
@@ -55,6 +77,12 @@ protected:
 
 	UFUNCTION()
 	void OnRep_PlayerRole();
+	
+	UFUNCTION()
+	void OnRep_VoiceChatState();
+
+	UFUNCTION()
+	void OnRep_VoiceObjectHeld();
 
 	UPROPERTY(EditDefaultsOnly, ReplicatedUsing = OnRep_LifeChanged, Category = "PS3|Player State", meta = (ClampMin = "0"))
 	int32 CurrentLifeCount = 3;
@@ -64,4 +92,10 @@ protected:
 
 	UPROPERTY(VisibleInstanceOnly, ReplicatedUsing = OnRep_PlayerRole, Category = "PS3|Player State")
 	EPS3PlayerRole PlayerRole = EPS3PlayerRole::Unassigned;
+	
+	UPROPERTY(VisibleInstanceOnly, ReplicatedUsing = OnRep_VoiceChatState, Category = "PS3|Player State|Voice")
+	EVoiceChatState VoiceChatState = EVoiceChatState::Default;
+
+	UPROPERTY(VisibleInstanceOnly, ReplicatedUsing = OnRep_VoiceObjectHeld, Category = "PS3|Player State|Voice")
+	bool bIsVoiceObjectHeld = false;
 };
