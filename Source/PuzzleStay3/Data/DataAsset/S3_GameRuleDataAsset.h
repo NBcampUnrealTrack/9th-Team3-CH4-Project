ed80360055b4004c15e4a53a81763e0353db958b
Noong <mw.noong@gmail.com>
2026-09-07 22:19:30 +0900

    Feat(core): Stage3 트랩/플레이어 상태 관리 및 델리게이트 추가
    무작위 시각 상태 계산, 델리게이트 방송, 트랩 및 플레이어 상태 등록 기능 구현.
    US3_GameRuleDataAsset을 통한 설정 관리 추가.
    ⚠ 여러 변경사항이 섞여있습니다 - 커밋 분리 권장

---
+++ Source/PuzzleStay3/Data/DataAsset/S3_GameRuleDataAsset.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "S3_GameRuleDataAsset.generated.h"
+
+class APS3PlayerCharacter;
+
+UCLASS()
+class PUZZLESTAY3_API US3_GameRuleDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:	
+	
+	UPROPERTY(EditAnywhere, Category = "GameRule")
+	int32 MaxPlayer = 2;
+	
+	UPROPERTY(EditAnywhere, Category = "GameRule")
+	int32 MaxFakeDeathTrap = 10;
+	
+
+};
