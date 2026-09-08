74f541f9ad782278677ab392772f8792ba7f5a9c
Hwang_Junhyun <h010617@naver.com>
2026-09-08 15:20:51 +0900

    feat(Player): 저울 컷씬 카메라 변환 수정 중

--- Source/PuzzleStay3/Player/Controller/PS3PlayerController.cpp
+++ Source/PuzzleStay3/Player/Controller/PS3PlayerController.cpp
@@ -1,6 +1,9 @@
 #include "PS3PlayerController.h"
 #include "Component/FakeDeathTrapComponent.h"
 #include "EngineUtils.h"
+#include "Object/Jeoul.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
@@ -52,6 +55,7 @@ void APS3PlayerController::BeginPlay()
 
 void APS3PlayerController::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
+	EndJeoulCutscene();
 	GetWorldTimerManager().ClearTimer(Stage3VisibilityTimerHandle);
 	GetWorldTimerManager().ClearTimer(LifeUIInitializationTimerHandle);
 
@@ -89,6 +93,7 @@ void APS3PlayerController::Client_PrepareForRespawn_Implementation()
 		return;
 	}
 
+	EndJeoulCutscene();
 	SetIgnoreMoveInput(true);
 	SetIgnoreLookInput(true);
 
@@ -105,6 +110,8 @@ void APS3PlayerController::Client_RestoreAfterRespawn_Implementation()
 		return;
 	}
 
+	EndJeoulCutscene();
+	if (IsValid(GetPawn())) SetViewTarget(GetPawn());
 	ResetIgnoreMoveInput();
 	ResetIgnoreLookInput();
 	ConfigureLocalInput();
@@ -266,6 +273,7 @@ void APS3PlayerController::SetupInputComponent()
 
 void APS3PlayerController::HandleMoveInput(const FInputActionValue& InValue)
 {
+	if (bJeoulCutsceneActive) return;
 	if (APS3PlayerCharacter* PlayerCharacter = GetPawn<APS3PlayerCharacter>())
 	{
 		PlayerCharacter->Move(InValue.Get<FVector2D>());
@@ -274,6 +282,7 @@ void APS3PlayerController::HandleMoveInput(const FInputActionValue& InValue)
 
 void APS3PlayerController::HandleLookInput(const FInputActionValue& InValue)
 {
+	if (bJeoulCutsceneActive) return;
 	if (APS3PlayerCharacter* PlayerCharacter = GetPawn<APS3PlayerCharacter>())
 	{
 		PlayerCharacter->Look(InValue.Get<FVector2D>());
@@ -282,6 +291,7 @@ void APS3PlayerController::HandleLookInput(const FInputActionValue& InValue)
 
 void APS3PlayerController::HandleJumpStarted()
 {
+	if (bJeoulCutsceneActive) return;
 	if (APS3PlayerCharacter* PlayerCharacter = GetPawn<APS3PlayerCharacter>())
 	{
 		PlayerCharacter->StartJump();
@@ -298,6 +308,11 @@ void APS3PlayerController::HandleJumpCompleted()
 
 void APS3PlayerController::HandleInteractStarted()
 {
+	if (bJeoulCutsceneActive)
+	{
+		EndJeoulCutscene();
+		return;
+	}
 	UE_LOG(LogTemp, Warning, TEXT("[Input] Interact pressed"));
 
 	if (APS3PlayerCharacter* PlayerCharacter = GetPawn<APS3PlayerCharacter>())
@@ -308,6 +323,7 @@ void APS3PlayerController::HandleInteractStarted()
 
 void APS3PlayerController::HandleDropStarted()
 {
+	if (bJeoulCutsceneActive) return;
 	UE_LOG(LogTemp, Warning, TEXT("[Input] Drop pressed"));
 
 	if (APS3PlayerCharacter* PlayerCharacter = GetPawn<APS3PlayerCharacter>())
@@ -319,6 +335,7 @@ void APS3PlayerController::HandleDropStarted()
 
 void APS3PlayerController::HandleVoiceStarted()
 {
+	if (bJeoulCutsceneActive) return;
 	UE_LOG(LogTemp, Warning, TEXT("Voice: V pressed"));
 	if (IsValid(VoiceComponent))
 	{
@@ -364,6 +381,60 @@ void APS3PlayerController::ApplyStage3Visibility()
 			}
 		}
 	}
+}
+
+void APS3PlayerController::Client_BeginJeoulCutscene_Implementation(AJeoul* Jeoul)
+{
+	if (!IsLocalController() || !IsValid(Jeoul)) return;
+	UCameraComponent* CutsceneCamera = Jeoul->GetCutsceneCamera();
+	if (!IsValid(CutsceneCamera) || !CutsceneCamera->IsActive()) return;
+	if (bJeoulCutsceneActive && ActiveCutsceneJeoul.Get() == Jeoul) return;
+	EndJeoulCutscene();
+
+	PreviousCutsceneViewTarget = GetViewTarget();
+	ActiveCutsceneJeoul = Jeoul;
+	bJeoulCutsceneActive = true;
+	SetIgnoreMoveInput(true);
+	SetIgnoreLookInput(true);
+	if (APS3PlayerCharacter* PlayerCharacter = GetPawn<APS3PlayerCharacter>())
+	{
+		PlayerCharacter->StopJump();
+		PlayerCharacter->GetCharacterMovement()->StopMovementImmediately();
+	}
+	if (IsValid(VoiceComponent)) VoiceComponent->StopPushToTalk();
+	JeoulCheckFinishedHandle = Jeoul->OnJeoulCheckFinished.AddUObject(this, &ThisClass::HandleJeoulCheckFinished);
+	Jeoul->OnDestroyed.AddUniqueDynamic(this, &ThisClass::HandleCutsceneTargetDestroyed);
+	SetViewTargetWithBlend(Jeoul, JeoulCameraBlendTime);
+}
+
+void APS3PlayerController::EndJeoulCutscene()
+{
+	if (!bJeoulCutsceneActive) return;
+	if (AJeoul* Jeoul = ActiveCutsceneJeoul.Get())
+	{
+		Jeoul->OnJeoulCheckFinished.Remove(JeoulCheckFinishedHandle);
+		Jeoul->OnDestroyed.RemoveDynamic(this, &ThisClass::HandleCutsceneTargetDestroyed);
+	}
+	JeoulCheckFinishedHandle.Reset();
+	ActiveCutsceneJeoul.Reset();
+	bJeoulCutsceneActive = false;
+	// 이 컷신에서 추가한 입력 잠금 한 번만 해제합니다.
+	SetIgnoreMoveInput(false);
+	SetIgnoreLookInput(false);
+	AActor* RestoreTarget = PreviousCutsceneViewTarget.Get();
+	if (!IsValid(RestoreTarget)) RestoreTarget = GetPawn();
+	if (IsValid(RestoreTarget)) SetViewTargetWithBlend(RestoreTarget, JeoulCameraBlendTime);
+	PreviousCutsceneViewTarget.Reset();
+}
+
+void APS3PlayerController::HandleJeoulCheckFinished(bool bIsSuccess)
+{
+	EndJeoulCutscene();
+}
+
+void APS3PlayerController::HandleCutsceneTargetDestroyed(AActor* DestroyedActor)
+{
+	EndJeoulCutscene();
 }
 
 bool APS3PlayerController::InitializeVoiceSystem(const int32 LocalUserNum)
