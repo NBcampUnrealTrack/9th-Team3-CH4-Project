7a7384d03e81230b1b47ea21b4ce0ce74805456a
Noong <mw.noong@gmail.com>
2026-09-10 18:18:40 +0900

    Feat(core): Stage별 데이터 에셋 초기화 함수 추가
    Stage1~4 GameState에 GameRule DataAsset 유효성 검사 및 초기화 함수 추가.
    ⚠ 기본 이름 그대로인 에셋 있음 - 이름 확인 필요

--- Source/PuzzleStay3/Core/GameState/PS3GameStateS1.cpp
+++ Source/PuzzleStay3/Core/GameState/PS3GameStateS1.cpp
@@ -18,10 +18,12 @@ void APS3GameStateS1::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	InitializeToDataAssets();
+		
 	SetUIMacroTimerHandle(
-		0.01,
+		0.1,
 		[this]() { PS3_BROADCAST_TO_MVVM_OneParams(OnStageType_UI, S1_GameRuleDataAsset->StageType_S1); });
-
+	
 	SetUIMacroTimerHandle(
 		0.5,
 		[this]() { PS3_BROADCAST_TO_MVVM_OneParams(OnTextNotify_UI, EPS3TextNotifyType::Stage1); });
@@ -29,6 +31,11 @@ void APS3GameStateS1::BeginPlay()
 	SetUIMacroTimerHandle(
 		0.7,
 		[this]() { PS3_BROADCAST_TO_MVVM_OneParams(OnTutorial_UI,true); });
+}
+
+void APS3GameStateS1::InitializeToDataAssets()
+{
+	checkf(IsValid(S1_GameRuleDataAsset) == true, TEXT("[APS3GameModeS1]의 데이터에셋이 비어있습니다."));
 }
 
 void APS3GameStateS1::SetStage1BlockingVolumeDisabled(bool bDisabled)
--- Source/PuzzleStay3/Core/GameState/PS3GameStateS3.h
+++ Source/PuzzleStay3/Core/GameState/PS3GameStateS3.h
@@ -17,7 +17,8 @@ class PUZZLESTAY3_API APS3GameStateS3 : public APS3GameStateBase
 	//BlockingVolume
 public:
 	virtual void BeginPlay() override;
-	
+	void InitializeToDataAssets();
+
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 	UFUNCTION(BlueprintPure)
