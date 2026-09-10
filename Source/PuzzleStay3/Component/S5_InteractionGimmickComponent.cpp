1bf8b9e0faf33c4d677392bef723715dae65ee94
mnns0805 <mnns0805@gmail.com>
2026-09-10 20:32:13 +0900

    feat: S5용도의 상호작용 컴포넌트 추가

--- Source/PuzzleStay3/Component/S5_InteractionGimmickComponent.cpp
+++ Source/PuzzleStay3/Component/S5_InteractionGimmickComponent.cpp
@@ -1,21 +1,41 @@
 ﻿#include "S5_InteractionGimmickComponent.h"
 
+#include "Net/UnrealNetwork.h"
+
 
 US5_InteractionGimmickComponent::US5_InteractionGimmickComponent()
 {
 
 }
 
+
+void US5_InteractionGimmickComponent::GetLifetimeReplicatedProps(
+	TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(ThisClass, bIsInteractedGimmick);
+	DOREPLIFETIME(ThisClass, bIsEscapeDoor);
+}
+
+
 bool US5_InteractionGimmickComponent::CanInteract_Implementation(AActor* Requestor) const
 {
-	return IPS3InteractableInterface::CanInteract_Implementation(Requestor);
+	return false;
 }
+
 
 bool US5_InteractionGimmickComponent::Interact_Implementation(AActor* Requestor)
 {
-	return IPS3InteractableInterface::Interact_Implementation(Requestor);
+	if (bIsEscapeDoor == false) return false;
+	if (!CanInteract_Implementation(Requestor))	return false;
 	
+	OnInteractedGimmick.Broadcast(bIsInteractedGimmick);
+	
+	return false;
 }
+
+
 
 
 
--- Source/PuzzleStay3/Component/S5_InteractionGimmickComponent.h
+++ Source/PuzzleStay3/Component/S5_InteractionGimmickComponent.h
@@ -5,6 +5,9 @@
 #include "Player/Interaction/PS3InteractableInterface.h"
 #include "S5_InteractionGimmickComponent.generated.h"
 
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnInteractedGimmick, bool);
+DECLARE_MULTICAST_DELEGATE(FOnCosmeticInteractionSuccessed);
+
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class PUZZLESTAY3_API US5_InteractionGimmickComponent : public UActorComponent , public IPS3InteractableInterface
@@ -14,8 +17,15 @@ class PUZZLESTAY3_API US5_InteractionGimmickComponent : public UActorComponent ,
 public:
 	US5_InteractionGimmickComponent();
 
-protected:
-	virtual bool CanInteract_Implementation(AActor* Requestor) const override;
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 	
+	virtual bool CanInteract_Implementation(AActor* Requestor) const override;
 	virtual bool Interact_Implementation(AActor* Requestor) override;
+	
+	UPROPERTY(Replicated)
+	bool bIsEscapeDoor = false;
+	UPROPERTY(Replicated)
+	bool bIsInteractedGimmick = false;
+	
+	FOnInteractedGimmick OnInteractedGimmick;
 };
