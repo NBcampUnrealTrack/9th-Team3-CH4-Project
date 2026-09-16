916c7eb90c5543ef9ce577b5a62c549b35db9d0f
Noong <mw.noong@gmail.com>
2026-09-16 17:55:06 +0900

    Feat(component): 문 차단 컴포넌트 추가 및 기능 연동
    문 상태에 따라 충돌 설정을 동적으로 변경하는 DoorBlockingVolumeComponent 추가.
    ⚠ 에셋과 코드 변경 분리 권장
    ⚠ 기본 이름 그대로인 에셋 있음 - 이름 확인 필요
    ⚠ 팀 공유 설정 변경 - 리뷰 필요

---
+++ Source/PuzzleStay3/Component/DoorBlockingVolumeComponent.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "DoorBlockingVolumeComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PUZZLESTAY3_API UDoorBlockingVolumeComponent : public UBoxComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UDoorBlockingVolumeComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetDoorFullyOpened(bool bOpened);
+
+	UFUNCTION(BlueprintCallable)
+	void SetBlockingEnabled(bool bEnabled);
+ㅈㅈㅇㅈ
+};
